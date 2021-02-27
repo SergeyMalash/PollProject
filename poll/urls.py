d9aa3f9d7e490e372699c39fcf5741dbc326e881
Sergey Malashenko <s.malash.dev@gmail.com>
2021-02-27 15:57:54 +0300

    fix urls.py and JWT

--- poll/urls.py
+++ poll/urls.py
@@ -1,29 +1,8 @@
-from django.conf.urls import url
 from django.urls import path
 from poll import views
 
-from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
-#
-# schema_view = get_schema_view(
-#    openapi.Info(
-#       title="Polls API",
-#       default_version='v1',
-#       description="Тестовый API для проведения опросов",
-#       terms_of_service="https://www.google.com/policies/terms/",
-#       contact=openapi.Contact(email="s.mallash.dev@gmail.com"),
-#       license=openapi.License(name="BSD License"),
-#    ),
-#    public=True,
-#    permission_classes=(permissions.AllowAny,),
-# )
-
 
 urlpatterns = [
-    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('polls/', views.AllPollsView.as_view()),
     path('polls/active/', views.ActivePollsView.as_view()),
     path('polls/<pk>/', views.PollView.as_view()),
@@ -37,6 +16,4 @@ urlpatterns = [
     path('start_poll/', views.StartPollView.as_view()),
     path('started_polls/<user>/', views.StartedPollsView.as_view()),
     path('started_polls/<user>/<pk>/', views.StartedPollView.as_view()),
-    # path('my_polls/<pk>', views.CompletedPollsView.as_view()),
-    # path('get_anon_user_token/', custom_obtain_token.TokenObtainPairView.as_view())
 ]
